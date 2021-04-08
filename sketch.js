595b4b530b550a602b15ccb223a7e51763bc69e0
krishang27112007 <66776404+krishang27112007@users.noreply.github.com>
2021-04-08 16:08:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var car,wall
+var speed,weight
+
+function setup() {
+  createCanvas(800,400);
+  speed=random(55,90)
+weight=random(400,1500)
+car= createSprite(50,200,50,50)
+wall=createSprite(1500,200,60,height/2)
+
+}
+
+function draw() {
+  background(0); 
+  
+  car.shapeColor=(245, 72, 66)
+  car.velocityX = speed ; 
+
+  if(wall.x - car.x < wall.width/2 + car.width/2) {
+      car.velocityX = 0 ;
+      var deformation = 0.5*weight*speed*speed /22500 ; 
+      if(deformation > 180 )
+      {
+        car.shapeColor = color(255,0,0);
+      }
+      if(deformation < 180 && deformation  > 100 ) 
+      {
+        car.shapeColor = color(230,230,0);
+      }
+      if(deformation < 100 ) 
+      {
+        car.shapeColor = color(0,255,0);
+      }
+    } 
+
+  drawSprites();
+}
+
+
+//1100(deal price)
+//1500(orignal price)
+//both above are counted a quaterly
